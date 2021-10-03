2d93f27087c52f6e2605cbdcb1894103496243de
ShashikaHarshana <88888333+ShashikaHarshana@users.noreply.github.com>
2021-10-04 02:51:43 +0530

    dsfasdf
    sdf

--- schemas/patient.schema.js
+++ schemas/patient.schema.js
@@ -1,31 +1,33 @@
-const mongoose = require("mongoose");
-const dbUtill = require("../dbUtill/utills");
+const mongoose = require('mongoose')
+const dbUtill = require('../dbUtill/utills')
 
-const patientSchema = new mongoose.Schema({
+const patientSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        max: 255,
+      type: String,
+      required: true,
+      max: 255
     },
     email: {
-        type: String,
-        required: true,
-        max: 255,
-        unique: true
+      type: String,
+      required: true,
+      max: 255,
+      unique: true
     },
     password: {
-        type: String,
-        required: true,
-        max: 1024,
-        min: 6,
+      type: String,
+      required: true,
+      max: 1024,
+      min: 6
     },
-    history: [
-        {key: String, value: String}
-    ],
+    history: [{ key: String, value: String }],
     dob: {
-        type: Date,
-        required: true
+      type: Date,
+      required: true
     }
-}, {timestamps: true});
+  },
+  { timestamps: true }
+)
 
-module.exports = mongoose.model(dbUtill.PATIENT, patientSchema);
+module.exports = mongoose.model(dbUtill.PATIENT, patientSchema)
+//sdfkjskldfjlksd
